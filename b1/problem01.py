93fb9ad4de34583421a0276d9223db5a1b442039
[strong1133] <[strong1133@naver.com]>
2021-03-05 14:52:25 +0900

    [백준문제1]

---
+++ b1/problem01.py
@@ -0,0 +1,12 @@
+# 항해99_알고리즘_2021-03-05, 백준번호:10869 _사칙연산 /by 정석진
+
+a, b = input().split()  # a, b 입력 받기
+
+a = int(a)
+b = int(b)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a % b)
