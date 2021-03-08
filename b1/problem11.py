8391c77b24120e13f4b75dbf10d04c49571e2c60
[strong1133] <[strong1133@naver.com]>
2021-03-09 00:47:58 +0900

    [asd]

--- b1/problem11.py
+++ b1/problem11.py
@@ -2,17 +2,21 @@
 import math
 
 
-def issosu(num):
+def issosu(num):  # 9
     if num == 1:
         return False
-    n = int(math.sqrt(num))
-    for v in range(2, n+1):
-        if num % v == 0:
+
+    n = int(math.sqrt(num))  # 3
+
+    for v in range(2, n+1):  # 2 ~ 4
+        if num % v == 0:  # 9 % 2 = 0
             return False
     return True
 
 
 a, b = map(int, input().split())
-for i in range(a, b+1):
-    if issosu(i):
-        print(i)
+for i in range(a, b):  # 3 ~10
+    if issosu(i):  # 3  , 4, 5 ,6
+        print(i)  # 3 5
+
+#3, 5, 7,
---
+++ b1/problem12.py
@@ -0,0 +1,15 @@
+# 항해99_알고리즘_2021-03-08, 백준번호: 11729_하노이탑../by 정석진
+
+
+def hanoi(n, s, e, o):
+    if n == 1:
+        print(s, e)
+        return
+    hanoi(n-1, s, o, e)
+    print(s, e)
+    hanoi(n-1, o, e, s)
+
+
+n = int(input())
+print((2**n) - 1)
+hanoi(n, 1, 3, 2)
