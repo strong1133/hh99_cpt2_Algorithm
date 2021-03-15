a267280c8288498572ac6d63582e71bdd7477d77
[strong1133] <strong1133@naver.com>
2021-03-16 00:48:34 +0900

    [asd]

--- b2/problem24.py
+++ b2/problem24.py
@@ -1,1 +1,8 @@
 # 항해99_알고리즘_2021-03-15 백준번호: 15650_N과 M 2../by 정석진
+
+from itertools import combinations
+n, m = map(int, input().split())
+a = combinations(range(1, n+1), m)
+
+for i in a:
+    print(*i)
