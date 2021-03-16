4bbe1ecc06dc0e8dd913ce6f4920e11e0834ac8c
[strong1133] <strong1133@naver.com>
2021-03-17 00:37:28 +0900

    [b2 ~29]

--- altari/demantion01/problem06.py
+++ altari/demantion01/problem06.py
@@ -1,1 +1,16 @@
-# 항해99_알고리즘_2021-03-16 백준번호: 1546_평균../by 정석진
+# 항해99_알고리즘_2021-03-16 백준번호: 8958_OX퀴즈../by 정석진
+
+r = int(input())
+for _ in range(r):
+    ox = input()
+    arr = [i for i in str(ox)]
+    s = 0
+    c = 1
+    for i in arr:
+        if i == 'O':
+            s += c
+            c += 1
+        else:
+            c = 1
+
+    print(s)
--- b2/problem29.py
+++ b2/problem29.py
@@ -1,1 +1,11 @@
 # 항해99_알고리즘_2021-03-15 백준번호: 2231_분해합../by 정석진
+
+r = int(input())
+res = 0
+for i in range(1, r+1):
+    d = list(map(int, str(i)))
+    s = i + sum(d)
+    if s == r:
+        res = i
+        break
+print(res)
