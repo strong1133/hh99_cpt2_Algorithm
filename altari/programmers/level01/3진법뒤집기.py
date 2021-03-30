a99dce10336c405ed0f38a49222e3b040ba2ab23
[strong1133] <strong1133@naver.com>
2021-03-31 00:05:59 +0900

    [yes]

--- altari/programmers/level01/3진법뒤집기.py
+++ altari/programmers/level01/3진법뒤집기.py
@@ -1,4 +1,24 @@
 # 프로그래머스_알고리즘_2021-03-30, level01_3진법 뒤집기../by 정석진
 
-def solution():
-    return
+def solution(n):
+    if n <= 3:
+        return n
+
+    arr1 = []
+    res = 0
+    while 1:
+        na = n % 3
+        arr1.append(na)
+        mok = n // 3
+        n = mok
+        if mok < 3:
+            arr1.append(mok)
+            break
+    l = len(arr1)-1
+    for i in arr1:
+        res += (i*(3**l))
+        l -= 1
+    return res
+
+
+print(solution(1))
